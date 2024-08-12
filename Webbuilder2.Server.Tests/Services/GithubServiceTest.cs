45ebbf5193daa67ee738480630cf04602b530d2e
joelyou20 <joelcribby@live.ca>
2024-08-11 18:10:43 -0700

    update error container styling

--- Webbuilder2.Server.Tests/Services/GithubServiceTest.cs
+++ Webbuilder2.Server.Tests/Services/GithubServiceTest.cs
@@ -1,14 +1,7 @@
 ﻿using Moq;
-using NUnit.Framework;
 using Octokit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebBuilder2.Server.Services;
 using WebBuilder2.Server.Services.Contracts;
-using WebBuilder2.Shared.Validation;
 
 namespace Webbuilder2.Server.Tests.Services;
 
@@ -33,13 +26,43 @@ public class GithubServiceTest
     }
 
     [Test]
-    public void GetRepositoriesAsync_ShouldReturnValidResponse()
+    public async Task GetRepositoriesAsync_Succeeds()
     {
+        IReadOnlyList<Repository> repos = new List<Repository>()
+        {
+            new(1234567),
+            new(7654321),
+            new(1212121)
+        };
+
         // Arrange
-        //_githubServiceMock.Setup(x => x.)
+        _githubClientMock.Setup(x => x.Repository.GetAllForCurrent()).ReturnsAsync(repos);
 
         // Act
+        var result = await _githubService.GetRepositoriesAsync();
 
         // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Count(), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void GetRepositoriesAsync_Fails_WhenGithubClientReturnsNull()
+    {
+        IReadOnlyList<Repository> repos = null!;
+
+        // Arrange
+        _githubClientMock.Setup(x => x.Repository.GetAllForCurrent()).ReturnsAsync(repos);
+
+        // Act
+        Assert.ThrowsAsync<NullReferenceException>(async () => await _githubService.GetRepositoriesAsync());
+
+        // Assert
+    }
+
+    [Test]
+    public async Task CreateRepoAsync_Succeeds()
+    {
+
     }
 }
